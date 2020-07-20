7ae62dfd9116646d0c7699918fc959e282440271
Stephan <stephan.altmann@gmx.net>
2020-07-20 14:50:43 +0200

    Add files via upload

---
+++ Get-FSMORoleHolder.ps1
@@ -0,0 +1,16 @@
+﻿Function Get-FSMORoleHolder {
+    #Get information from domain and forest
+    $InfRidPdc = Get-ADDomain | Select-Object InfrastructureMaster, RIDMaster, PDCEmulator
+    $DnSch = Get-ADForest | Select-Object DomainNamingMaster, SchemaMaster
+
+    #Create new object with FSMO properties
+    $result = New-Object -TypeName System.Object
+    $result | Add-Member -MemberType NoteProperty -Name "InfrastructureMaster" -Value $InfRidPdc.InfrastructureMaster
+    $result | Add-Member -MemberType NoteProperty -Name "RIDMaster" -Value $InfRidPdc.RIDMaster
+    $result | Add-Member -MemberType NoteProperty -Name "PDCEmulator" -Value $InfRidPdc.PDCEmulator
+    $result | Add-Member -MemberType NoteProperty -Name "DomainNamingMaster" -Value $DnSch.DomainNamingMaster
+    $result | Add-Member -MemberType NoteProperty -Name "SchemaMaster" -Value $DnSch.SchemaMaster
+
+    #Return results
+    return $result
+}
